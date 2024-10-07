9592847f07f800a0667c601dab268c305e8b81a9
ezedin ebrahim <ezedinebrahim131@gmail.com>
2024-10-07 10:53:40 +0300

    deleting cors

--- server.js
+++ server.js
@@ -9,29 +9,6 @@ connectDb();
 const app = express();
 
 // Load allowed origins from environment variables
-const allowedOrigins = process.env.ALLOWED_ORIGINS
-  ? process.env.ALLOWED_ORIGINS.split(",")
-  : [];
-
-// CORS options
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      // Allow if the origin is in the list or it's not provided (like with curl or Postman)
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true,
-};
-
-// Enable CORS
-app.use(cors(corsOptions));
-
-// Handle preflight requests globally
-app.options("*", cors(corsOptions));
 
 const port = process.env.PORT || 3001; // Use the port defined in the environment variable
 app.use(express.json());
